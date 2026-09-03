7e58b494d10c4cb33f11bc9d58cf3f799a0a35df
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-03 18:13:20 +0800

    Update CorsConfig.java

--- LegalOffice/src/main/java/com/anuge/legaloffice/config/CorsConfig.java
+++ LegalOffice/src/main/java/com/anuge/legaloffice/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
         // Allow Angular application hosted on Vercel
         configuration.setAllowedOrigins(List.of(
-            "https://legaloffice-management.vercel.app"
+            "https://legaloffice.vercel.app"
         ));
 
         // Allow HTTP methods used by the Angular application
