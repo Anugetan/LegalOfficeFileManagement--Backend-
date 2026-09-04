aea6d319ececb9432564da6e45c99b2e048e8532
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-04 16:04:05 +0800

    Add files via upload
    FileUpload

---
+++ LegalOffice/src/main/java/com/anuge/legaloffice/dto/FileUploadResponse.java
@@ -0,0 +1,97 @@
+package com.anuge.legaloffice.dto;
+
+import java.time.LocalDateTime;
+
+public class FileUploadResponse {
+
+    private Long id;
+    private Long fileId;
+    private String documentName;
+    private String fileName;
+    private Long fileSize;
+    private String mimeType;
+    private Integer version;
+    private Boolean isFinal;
+    private String uploadedBy;
+    private LocalDateTime uploadedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(Long fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Long fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Boolean getIsFinal() {
+        return isFinal;
+    }
+
+    public void setIsFinal(Boolean isFinal) {
+        this.isFinal = isFinal;
+    }
+
+    public String getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(String uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+}
