3669b6d133ebb4286cbf54c2724c4c2b1732b728
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-04 16:05:10 +0800

    Add files via upload
    FileUpload

---
+++ LegalOffice/src/main/java/com/anuge/legaloffice/repository/FileDocumentRepository.java
@@ -0,0 +1,12 @@
+package com.anuge.legaloffice.repository;
+
+import com.anuge.legaloffice.entity.FileDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FileDocumentRepository
+        extends JpaRepository<FileDocument, Long> {
+
+    List<FileDocument> findByLegalFileId(Long fileId);
+}
