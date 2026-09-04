b994d58fe3bb7dbf4b525c0727a13764898936c6
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-04 16:17:05 +0800

    Update FileDocumentRepository.java

--- LegalOffice/src/main/java/com/anuge/legaloffice/repository/FileDocumentRepository.java
+++ LegalOffice/src/main/java/com/anuge/legaloffice/repository/FileDocumentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface FileDocumentRepository
         extends JpaRepository<FileDocument, Long> {
 
