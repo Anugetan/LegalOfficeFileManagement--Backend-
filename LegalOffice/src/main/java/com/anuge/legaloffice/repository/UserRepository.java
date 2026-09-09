01821aee5342e9519ef8d84593287f6992d9ce02
Jonathan Eguna <nath9neguna@gmail.com>
2026-09-09 15:53:53 +0800

    Update UserRepository.java
    List of Users

--- LegalOffice/src/main/java/com/anuge/legaloffice/repository/UserRepository.java
+++ LegalOffice/src/main/java/com/anuge/legaloffice/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.anuge.legaloffice.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.anuge.legaloffice.entity.Users;
+import com.anuge.legaloffice.enumerate.RegistrationStatus;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
@@ -13,4 +15,8 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+    
+    List<Users> findByRegistrationStatus(
+            RegistrationStatus registrationStatus
+    );
 }
